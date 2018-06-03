6c11829a83a9b3797cb8c84e19054558a13989af
yanglibo <1453463056@qq.com>
2018-06-03 21:58:25 +0800

    增加最大子序列和的扫描法解法，详见《编程珠玑》8.4节。

--- src/MaxSubArray.java
+++ src/MaxSubArray.java
@@ -20,7 +20,16 @@ public class MaxSubArray{
     //扫描法，复杂度O(n)
     public int scanSolution(int[] nums){
         for(int i=0; i < nums.length; i++){
-
+            int current=nums[0];
+            int sum=nums[0];
+            //我们考虑如果全是负数，那么返回最大的负数，如果最后的和为正，那么就使用扫描法
+            for(i=1;i<nums.length;i++) {
+                if(current<0)current=nums[i];//当前数小于0 肯定会舍去（否则将会影响接下来的和），换为下一个数
+                else current+=nums[i];//如果当前数不小于0，那么他会对接下来的和有积极影响
+                if(current>sum)sum=current;//这里既实现了负数返回最大也实现了扫描法
+                //这里其实已经隐式的列举了所有可能，保留了所有可能的最大值
+            }
+            return sum;
         }
         return max;
     }
