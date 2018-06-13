d5d9f8ef9eea829d2b50d75483add2d93ff8a3f4
yanglibo <1453463056@qq.com>
2018-06-13 22:22:52 +0800

    添加最长不重复子串的暴力解法

---
+++ src/LongestSubstring_3.java
@@ -0,0 +1,60 @@
+import java.util.*;
+
+
+/**
+ * 给定一个字符串，找出不含有重复字符的最长子串的长度。
+
+ 示例：
+
+ 给定 "abcabcbb" ，没有重复字符的最长子串是 "abc" ，那么长度就是3。
+
+ 给定 "bbbbb" ，最长的子串就是 "b" ，长度是1。
+
+ 给定 "pwwkew" ，最长子串是 "wke" ，长度是3。请注意答案必须是一个子串，"pwke" 是 子序列  而不是子串。
+ */
+
+public class LongestSubstring_3 {
+    public static void main(String[] args){
+        List<Integer> a = new LinkedList<>(Arrays.asList(1,2,3,4));
+        System.out.println(a);
+    }
+
+    public int lengthOfLCS(String s) {
+        for(int i=0; i<s.length(); i++){
+            char sub = s.charAt(i);
+        }
+        return 0;
+    }
+
+    //暴力解法超时,简单减少最开始循环勉强通过
+    public int useHashset(String s) {
+        int max = Integer.MIN_VALUE;
+        HashSet<Character> sset = new HashSet<Character>(){{
+            for (char a:s.toCharArray()){
+                add(a);
+            }
+        }
+        };
+
+        for (int i = 0; i <= sset.size(); i++) {
+            for (int j=0; j<= s.length() - i; j++){
+                String substr = s.substring(j, j+i);
+                HashSet<Character> stringset = new HashSet<Character>(){{
+                    for (char a:substr.toCharArray()){
+                        add(a);
+                    }
+                }
+                };
+
+                if(stringset.size() == i){
+                    max = Integer.max(max, i);
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+}
+
+
+
--- src/MaxSubArray_53.java
+++ src/MaxSubArray_53.java
@@ -1,4 +1,14 @@
-public class MaxSubArray{
+/**
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+
+ 示例:
+
+ 输入: [-2,1,-3,4,-1,2,1,-5,4],
+ 输出: 6
+ 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ */
+
+public class MaxSubArray_53 {
     public int max = Integer.MIN_VALUE;
 
     //暴力算法，直接计算，复杂度O(n^2)
@@ -75,7 +85,7 @@ public class MaxSubArray{
 
     public static void main(String[] args){
         int[] nums = {-1,2,-3,-4,5};
-        MaxSubArray results = new MaxSubArray();
+        MaxSubArray_53 results = new MaxSubArray_53();
         System.out.println(results.reduceSolution(nums, 0, nums.length-1));
     }
 }
