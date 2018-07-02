6eb0b6d715702d40ca493903946d09298ac95015
yanglibo <1453463056@qq.com>
2018-07-02 11:55:54 +0800

    面试题8：在旋转数组中搜索最小数

--- src/SwordPointOffer/O8_SearchRotateArray.java
+++ src/SwordPointOffer/O8_SearchRotateArray.java
@@ -10,6 +10,9 @@ public class O8_SearchRotateArray {
                 return array[right];
             }
             mid = (left+right)/2;
+            if(array[left] == array[right] && array[mid] == array[left])
+                return minInOrder(array, left, right);
+
             if(array[mid] >= array[left]){
                 left = mid;
             }
@@ -18,6 +21,14 @@ public class O8_SearchRotateArray {
             }
         }
         return array[mid];
+    }
+
+    private static int minInOrder(int[] array, int left, int right){
+        int min = array[left];
+        for(int i=left+1; i<=right; i++){
+            min = Integer.min(min, array[i]);
+        }
+        return min;
     }
 
     public static void main(String[] args){
